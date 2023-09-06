8015a8e1ba6e2006deb075415a9c7122ca576619
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-09-06 15:58:26 +0200

    Accordions html, css, js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+	const accordion_toggle = accordion.querySelector('.accordion__toggle');
+	const accordion_content = accordion.querySelector('.accordion__content');
+
+	accordion_toggle.addEventListener('click', () => {
+		accordion_content.classList.toggle('accordion__content--visible')		
+	});
+});
