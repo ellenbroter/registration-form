d7453cc1206016228721826bbdd73b38e180f4d0
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-09-08 13:25:38 +0200

    Checkbox javascript selectAll = true

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ accordions.forEach(accordion => {
 	});
 });
 
+function selectAll() {
+	var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+	 for (var i = 0; i < checkboxes.length; i++) {
+	  if (checkboxes[i].type == 'checkbox')
+		 checkboxes[i].checked = true;
+	 }
+}
